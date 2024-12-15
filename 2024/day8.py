f136342bd319428b29d3f99d6de6949b4f0c40cf
Natalya Smolko <n.smolko@bankingon.io>
2024-12-16 00:30:00 +0300

    v3

--- 2024/day8.py
+++ 2024/day8.py
@@ -17,11 +17,15 @@ for i in range(height):
 
 # print(antennas_cords)
 
+def make_step(coords: tuple, step: tuple, left: bool = True, inverted_diag: bool = False) -> tuple:
+    if left:
+        return coords[0] - step(0), coords[1] + step[1] * (-1 + 2 * inverted_diag)
+
 def check_antinode_fits_borders(antinode: tuple) -> bool:
     return 0 <= antinode[0] < height and 0 <= antinode[1] < width
 
 
-def calculate_antinode(one: tuple, two: tuple, to_borders: bool = False) -> list:
+def calculate_antinode(one: tuple, two: tuple, to_borders: bool = False) -> set:
     one, two = sorted([one, two])
 
     y1, x1 = one
@@ -30,37 +34,14 @@ def calculate_antinode(one: tuple, two: tuple, to_borders: bool = False) -> list
     y_diff = y1 - y2
     x_diff = x1 - x2
 
-    res = []
+    res = set()
     if y_diff <= 0 and x_diff <= 0:
-        print_pink('normal diagonal')
         left, right = ((y1 - abs(y_diff), x1 - abs(x_diff)), (y2 + abs(y_diff), x2 + abs(x_diff)))
-        if to_borders:
-            while check_antinode_fits_borders(left):
-                print(f'left is correct: {left}')
-                res.append(left)
-                left = (left[0] - abs(y_diff), left[1] - abs(x_diff))
-            while check_antinode_fits_borders(right):
-                print(f'right is correct: {right}')
-                res.append(right)
-                right = (right[0] + abs(y_diff), right[1] + abs(x_diff))
-        else:
-            res.extend([left, right])
+        res.update([left, right])
     else:
-        print_pink('inverted diagonal')
         left, right = ((y1 - abs(y_diff), x1 + abs(x_diff)), (y2 + abs(y_diff), x2 - abs(x_diff)))
-        if to_borders:
-            while check_antinode_fits_borders(left):
-                print(f'left is correct: {left}')
-                res.append(left)
-                left = (left[0] - abs(y_diff), left[1] + abs(x_diff))
-            while check_antinode_fits_borders(right):
-                print(f'right is correct: {right}')
-                res.append(right)
-                right = (right[0] + abs(y_diff), right[1] - abs(x_diff))
-        else:
-            res.extend([left, right])
+        res.update([left, right])
     return res
-
 
 
 final = set()
@@ -70,18 +51,21 @@ for k, v in antennas_cords.items():
         new_atinodes = list(filter(check_antinode_fits_borders, new_atinodes))
         final.update(new_atinodes)
 
+print(sorted(final))
 print_pink(f"*** puzzle 1: {len(final)} ***")
+# [(0, 6), (0, 11), (1, 3), (2, 4), (2, 10), (3, 2), (4, 9), (5, 1), (5, 6), (6, 3), (7, 0), (7, 7), (10, 10), (11, 10)]
 
 final2 = set()
-for k, v in antennas_cords.items():
-    print_pink(f"*** {k} ***")
-    for opt in combinations(v, 2):
-        print_blue(opt)
-        new_atinodes = calculate_antinode(*opt, to_borders=True)
-        print('new:', len(new_atinodes), new_atinodes)
-        new_atinodes = list(filter(check_antinode_fits_borders, new_atinodes))
-        print('filtered', len(new_atinodes), new_atinodes)
-        final2.update(new_atinodes)
+# for k, v in antennas_cords.items():
+#     print_pink(f"*** {k} ***")
+#     for opt in combinations(v, 2):
+#         print_blue(opt)
+#         new_atinodes = calculate_antinode(*opt, to_borders=True)
+#         print('new:', len(new_atinodes), new_atinodes)
+#         new_atinodes = list(filter(check_antinode_fits_borders, new_atinodes))
+#         print('filtered', len(new_atinodes), new_atinodes)
+#         final2.update(new_atinodes)
 
 
 print_pink(f"*** puzzle 2: {len(final2)} ***")
+# 1125 - too low
