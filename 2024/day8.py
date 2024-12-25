95ea47c57426056d8457bda2df2840632d8f0902
Natalya Smolko <n.smolko@bankingon.io>
2024-12-25 12:17:03 +0200

    day8 final

--- 2024/day8.py
+++ 2024/day8.py
@@ -15,8 +15,6 @@ for i in range(height):
         if data[i][j] != '.':
             antennas_cords.setdefault(data[i][j], []).append((i, j))
 
-# print(antennas_cords)
-
 def make_step(coords: tuple, step: tuple, left: bool = True, inverted_diag: bool = False) -> tuple:
     if left:
         return coords[0] - step[0], coords[1] + step[1] * (-1 + 2 * inverted_diag)
@@ -43,18 +41,10 @@ def calculate_antinode(one: tuple, two: tuple) -> set:
     right = make_step(two, step, left=False, inverted_diag=inverted_diag)
     res.update([left, right])
 
-    # if y_diff <= 0 and x_diff <= 0:
-    #     left, right = ((y1 - abs(y_diff), x1 - abs(x_diff)), (y2 + abs(y_diff), x2 + abs(x_diff)))
-    #     res.update([left, right])
-    # else:
-    #     left, right = ((y1 - abs(y_diff), x1 + abs(x_diff)), (y2 + abs(y_diff), x2 - abs(x_diff)))
-    #     res.update([left, right])
     return res
 
 def calculate_all_antinodes(one: tuple, two: tuple) -> set:
     one, two = sorted([one, two])
-
-    print_blue(f"*** {one} - {two} ***")
 
     y1, x1 = one
     y2, x2 = two
@@ -76,8 +66,6 @@ def calculate_all_antinodes(one: tuple, two: tuple) -> set:
         res.add(right)
         right = make_step(right, step, left=False, inverted_diag=inverted_diag)
 
-    print(res)
-
     if len(res) >= 2:
         return res
     return set()
@@ -89,9 +77,8 @@ for k, v in antennas_cords.items():
         new_atinodes = list(filter(check_antinode_fits_borders, new_atinodes))
         final.update(new_atinodes)
 
-# print(sorted(final))
 print_pink(f"*** puzzle 1: {len(final)} ***")
-# [(0, 6), (0, 11), (1, 3), (2, 4), (2, 10), (3, 2), (4, 9), (5, 1), (5, 6), (6, 3), (7, 0), (7, 7), (10, 10), (11, 10)]
+
 
 final2 = set()
 for k, v in antennas_cords.items():
@@ -100,7 +87,4 @@ for k, v in antennas_cords.items():
         new_atinodes = list(filter(check_antinode_fits_borders, new_atinodes))
         final2.update(new_atinodes)
 
-
 print_pink(f"*** puzzle 2: {len(final2)} ***")
-
-# 341 1134
